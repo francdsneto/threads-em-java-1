d0c1c355d860973b9da3267a0b666230205ce91a
Neto <setorti01@comshalom.org>
2024-01-09 19:31:34 -0300

    Primeiro commit da aplicação utilizada para acompanhar o conteúdo sobre Threads do Curso 'Threads em Java 1: Programação paralela

---
+++ src/main/java/estudo/curso/alura/threads/Principal.java
@@ -0,0 +1,13 @@
+package estudo.curso.alura.threads;
+
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Essa é a thread main");
+
+        Thread.sleep(60000);
+
+    }
+
+}
